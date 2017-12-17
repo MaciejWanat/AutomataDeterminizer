4c14bc33b6c8406ba8d9dc239c8ce6679d9dca27
Maciej Wanat <maciejwanat@10g.pl>
2017-12-17 22:19:23 +0100

    Supports determine automaton on input

--- determinize.py
+++ determinize.py
@@ -37,78 +37,79 @@ for line in sys.stdin:
 		else:
 			acceptStates.append(line.strip())
 
-
-maxDet = min(nonDetWalks)
 determineAutomaton = dict(automaton)
-i = 0
-#Determine into multistates----
-#while you can find any unmapped multistate
-while foundState == True:
-    i = i + 1
-    print('----------------')
-    print('Iteration ' + str(i))
-    prettyPrint(determineAutomaton)
-    print()
-    #iterate through 'automaton', add new states to 'determineAutomaton'.
-    #if you add anything, check if there is any new unmapped state.
-    foundState = False
-    automaton = dict(determineAutomaton)
-    #for each state-symbol pair...
-    for key in automaton:
-        #check if it is a multistate (can get more than one state), and if it is
-        if len(automaton[key]) > 1 and (frozenset(automaton[key]), key[-1]) not in automaton:
-            print('Found multistate : ' + str(automaton[key]) + ". Adding to automaton...")
-            foundState = True
-            #for each symbol in alphabet...
-            for symbol in alphabet:
-                valueAutoma = set()
-                #for each state in multstational state...
-                for state in automaton[key]:
-                    #add states you can get to from single state and current symbol, if the connection exists
-                    if (state, symbol) in automaton:
-                        valueAutoma.update(automaton[(state, symbol)])
 
-                keyAutoma = (frozenset(automaton[key]), symbol)
-                determineAutomaton[keyAutoma] = valueAutoma
+#if the input automaton is nondeterministic
+if len(nonDetWalks):
+	maxDet = min(nonDetWalks)	
+	i = 0
+	#Determine into multistates----
+	#while you can find any unmapped multistate
+	while foundState == True:
+	    i = i + 1
+	    print('----------------')
+	    print('Iteration ' + str(i))
+	    prettyPrint(determineAutomaton)
+	    print()
+	    #iterate through 'automaton' and add new states to 'determineAutomaton'.
+	    #if you add anything, check if there is any new unmapped state.
+	    foundState = False
+	    automaton = dict(determineAutomaton)
+	    #for each state-symbol pair...
+	    for key in automaton:
+	        #check if it is a multistate (can get more than one state), and if it is
+	        if len(automaton[key]) > 1 and (frozenset(automaton[key]), key[-1]) not in automaton:
+	            print('Found multistate : ' + str(automaton[key]) + ". Adding to automaton...")
+	            foundState = True
+	            #for each symbol in alphabet...
+	            for symbol in alphabet:
+	                valueAutoma = set()
+	                #for each state in multstational state...
+	                for state in automaton[key]:
+	                    #add states you can get to from single state and current symbol, if the connection exists
+	                    if (state, symbol) in automaton:
+	                        valueAutoma.update(automaton[(state, symbol)])
 
+	                keyAutoma = (frozenset(automaton[key]), symbol)
+	                determineAutomaton[keyAutoma] = valueAutoma
 
-#Delete no longer needed single states
-for key in automaton:
-	if isinstance(key[0], str) and len(determineAutomaton[key]) == 1 and int(key[0]) > maxDet:
-		del determineAutomaton[key]
+	#Delete no longer needed single states
+	for key in automaton:
+		if isinstance(key[0], str) and len(determineAutomaton[key]) == 1 and int(key[0]) > maxDet:
+			del determineAutomaton[key]
 
-automaton = determineAutomaton
-orderedSingleAutomaton = {}
+	automaton = determineAutomaton
+	orderedSingleAutomaton = {}
 
-#Create map
-i = 0
-keysMap = {}
+	#Create map
+	i = 0
+	keysMap = {}
 
-#Map single states
-while i <= maxDet:
-	keysMap[str(i)] = str(i)
-	i = i + 1
+	#Map single states
+	while i <= maxDet:
+		keysMap[str(i)] = str(i)
+		i = i + 1
 
-#Map multistates
-for key in determineAutomaton:
-    if key[0] not in keysMap:
-        keysMap[key[0]] = str(i)
-        i = i + 1
+	#Map multistates
+	for key in determineAutomaton:
+	    if key[0] not in keysMap:
+	        keysMap[key[0]] = str(i)
+	        i = i + 1
 
-print("--------\nMap:")
-prettyPrint(keysMap)
-print()
+	print("--------\nMap:")
+	prettyPrint(keysMap)
+	print()
 
-determineAutomaton = {}
-#Map values
-for key in automaton:
-    #transfer value set into frozenset, if it is a set or into single element if it is a single element
-    if len(automaton[key]) > 1:
-        determineAutomaton[(keysMap[key[0]], key[1])] = keysMap[frozenset(automaton[key])]
-    else:
-        for e in automaton[key]:
-            singleElement = e
-        determineAutomaton[(keysMap[key[0]], key[1])] = keysMap[singleElement]
+	determineAutomaton = {}
+	#Map values
+	for key in automaton:
+	    #transfer value set into frozenset, if it is a set or into single element if it is a single element
+	    if len(automaton[key]) > 1:
+	        determineAutomaton[(keysMap[key[0]], key[1])] = keysMap[frozenset(automaton[key])]
+	    else:
+	        for e in automaton[key]:
+	            singleElement = e
+	        determineAutomaton[(keysMap[key[0]], key[1])] = keysMap[singleElement]
 
 print("Final determine automaton:")
 prettyPrintOrdered(determineAutomaton)
